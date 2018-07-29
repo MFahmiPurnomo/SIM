f0a8627c57b19e2503fb5608f6dedc028ade5829
MFahmiPurnomo <mfahmipurnomo@gmail.com>
2018-07-29 22:23:48 +0700

    Dari Awal sampai Data Anggota

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,252 @@
+<!doctype html>
+<html lang="">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Propeller Admin Dashboard">
+<meta content="width=device-width, initial-scale=1, user-scalable=no" name="viewport">
+
+<title>SI Rapat</title>
+<meta name="description" content="Sistem informasi rapat jurusan teknik informatika."/>
+
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('admin/themes/images/favicon.ico') }}">
+
+<!-- Google icon -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+<!-- Bootstrap css -->
+<link rel="stylesheet" type="text/css" href="{{asset('admin/assets/css/bootstrap.min.css')}}">
+
+<!-- Propeller css -->
+<!-- build:[href] assets/css/ -->
+<link rel="stylesheet" type="text/css" href="{{asset('admin/assets/css/propeller.min.css')}}">
+<!-- /build -->
+
+<!-- Propeller date time picker css-->
+<link rel="stylesheet" type="text/css" href="{{asset('admin/components/datetimepicker/css/bootstrap-datetimepicker.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{asset('admin/components/datetimepicker/css/pmd-datetimepicker.css')}}" />
+
+<!-- DataTables css-->
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.1.0/css/responsive.bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/select/1.2.0/css/select.dataTables.min.css">
+
+<!-- Propeller dataTables css-->
+<link rel="stylesheet" type="text/css" href="{{asset('admin/components/data-table/css/pmd-datatable.css')}}">
+
+<!-- Propeller theme css-->
+<link rel="stylesheet" type="text/css" href="{{asset('admin/themes/css/propeller-theme.css')}}" />
+
+<!-- Propeller admin theme css-->
+<link rel="stylesheet" type="text/css" href="{{asset('admin/themes/css/propeller-admin.css')}}">
+
+</head>
+
+<body>
+<!-- Header Starts -->
+<!--Start Nav bar -->
+<nav class="navbar navbar-inverse navbar-fixed-top pmd-navbar pmd-z-depth">
+
+    <div class="container-fluid">
+        <div class="pmd-navbar-right-icon pull-right navigation">
+            <!-- Notifications -->
+            {{-- <div class="dropdown notification icons pmd-dropdown">
+            
+                <a href="javascript:void(0)" title="Notification" class="dropdown-toggle pmd-ripple-effect"  data-toggle="dropdown" role="button" aria-expanded="true">
+                    <div data-badge="3" class="material-icons md-light pmd-sm pmd-badge  pmd-badge-overlap">notifications_none</div>
+                </a>
+            
+                <div class="dropdown-menu dropdown-menu-right pmd-card pmd-card-default pmd-z-depth" role="menu">
+                    <!-- Card header -->
+                    <div class="pmd-card-title">
+                        <div class="media-body media-middle">
+                            <a href="notifications.html" class="pull-right">3 new notifications</a>
+                            <h3 class="pmd-card-title-text">Notifications</h3>
+                        </div>
+                    </div>
+                    
+                    <!-- Notifications list -->
+
+                    <ul class="list-group pmd-list-avatar pmd-card-list">
+                        <li class="list-group-item" style="display:none">
+                            <p class="notification-blank">
+                                <span class="dic dic-notifications-none"></span> 
+                                <span>You don´t have any notifications</span>
+                            </p>
+                        </li>
+                        <li class="list-group-item unread">
+                            <a href="javascript:void(0)">
+                                <div class="media-left">
+                                    <span class="avatar-list-img40x40">
+                                        <img alt="40x40" data-src="holder.js/40x40" class="img-responsive" src="{{asset('admin/themes/images/profile-1.png')}}" data-holder-rendered="true">
+                                    </span>
+                                </div>
+                                <div class="media-body">
+                                    <span class="list-group-item-heading"><span>Prathit</span> posted a new challanegs</span>
+                                    <span class="list-group-item-text">5 Minutes ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="javascript:void(0)">
+                                <div class="media-left">
+                                    <span class="avatar-list-img40x40">
+                                        <img alt="40x40" data-src="holder.js/40x40" class="img-responsive" src="{{asset('admin/themes/images/profile-2.png')}}" data-holder-rendered="true">
+                                    </span>
+                                </div>
+                                <div class="media-body">
+                                    <span class="list-group-item-heading"><span>Keel</span> Cloned 2 challenges.</span>
+                                    <span class="list-group-item-text">15 Minutes ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="list-group-item unread">
+                            <a href="javascript:void(0)">
+                                <div class="media-left">
+                                    <span class="avatar-list-img40x40">
+                                        <img alt="40x40" data-src="holder.js/40x40" class="img-responsive" src="{{asset('admin/themes/images/profile-3.png')}}" data-holder-rendered="true">
+                                    </span>
+                                </div>
+                            
+                                <div class="media-body">
+                                    <span class="list-group-item-heading"><span>John</span> posted new collection.</span>
+                                    <span class="list-group-item-text">25 Minutes ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="list-group-item unread">
+                            <a href="javascript:void(0)">
+                                <div class="media-left">
+                                    <span class="avatar-list-img40x40">
+                                        <img alt="40x40" data-src="holder.js/40x40" class="img-responsive" src="{{asset('admin/themes/images/profile-4.png')}}" data-holder-rendered="true">
+                                    </span>
+                                </div>
+                                <div class="media-body">
+                                    <span class="list-group-item-heading"><span>Valerii</span> Shared 5 collection.</span>
+                                    <span class="list-group-item-text">30 Minutes ago</span>
+                                </div>
+                            </a>
+                        </li>
+                    </ul><!-- End notifications list -->
+
+                </div>
+                
+                
+            </div> <!-- End notifications --> --}}
+        </div>
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <a href="javascript:void(0);" data-target="basicSidebar" data-placement="left" data-position="slidepush" is-open="true" is-open-width="1200" class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect pull-left margin-r8 pmd-sidebar-toggle"><i class="material-icons md-light">menu</i></a>   
+            <a href="index.html" class="navbar-brand">
+                SIRapat
+            </a>
+        </div>
+    </div>
+
+</nav><!--End Nav bar -->
+<!-- Header Ends -->
+
+<!-- Sidebar Starts -->
+<div class="pmd-sidebar-overlay"></div>
+
+<!-- Left sidebar -->
+<aside id="basicSidebar" class="pmd-sidebar  sidebar-default pmd-sidebar-slide-push pmd-sidebar-left pmd-sidebar-open bg-fill-darkblue sidebar-with-icons" role="navigation">
+    <ul class="nav pmd-sidebar-nav">
+        
+        <!-- User info -->
+        <li class="dropdown pmd-dropdown pmd-user-info visible-xs visible-md visible-sm visible-lg">
+            <a aria-expanded="false" data-toggle="dropdown" class="btn-user dropdown-toggle media" data-sidebar="true" aria-expandedhref="javascript:void(0);">
+                <div class="media-left">
+                    <img src="{{asset('admin/themes/images/user-icon.png')}}" alt="New User">
+                </div>
+                <div class="media-body media-middle">{{Auth::user()->name}}</div>
+                <div class="media-right media-middle"><i class="dic-more-vert dic"></i></div>
+            </a>
+            <ul class="dropdown-menu">
+                <li class="nav-item dropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+            </ul>
+        </li><!-- End user info -->
+        
+        <li> 
+            <a class="pmd-ripple-effect" href="{{url('/home')}}"> 
+                <i class="media-left media-middle"><svg version="1.1" x="0px" y="0px" width="19.83px" height="18px" viewBox="287.725 407.535 19.83 18" enable-background="new 287.725 407.535 19.83 18" xml:space="preserve">
+                <g> <path fill="#C9C8C8" d="M307.555,407.535h-9.108v10.264h9.108V407.535z M287.725,407.535v6.232h9.109v-6.232H287.725z M296.834,415.271h-9.109v10.264h9.109V415.271z M307.555,419.303h-9.108v6.232h9.108V419.303z"/></g></svg></i>
+                <span class="media-body">Beranda</span>
+            </a> 
+        </li>
+        <li> 
+            <a class="pmd-ripple-effect" href="{{ url('anggota/index') }}"> 
+                <i class="media-left media-middle"><svg version="1.1" x="0px" y="0px" width="18px" height="18.001px" viewBox="0 0 18 18.001" enable-background="new 0 0 18 18.001" xml:space="preserve"> <path fill="#C9C8C8" d="M6.188,0.001C5.232,0.001,4.5,0.732,4.5,1.688c0,0.394,0.166,0.739,0.334,1.02L5.45,3.71 c0.113,0.113,0.176,0.341,0.176,0.51v0.281c0,0.619-0.506,1.125-1.125,1.125H0.282c-0.169,0-0.281,0.112-0.281,0.281V17.72    c0,0.168,0.112,0.281,0.281,0.281h4.219c0.619,0,1.125-0.506,1.125-1.125v-0.281c0-0.168-0.063-0.397-0.176-0.509   c0,0-0.615-0.946-0.615-1.002C4.666,14.802,4.5,14.457,4.5,14.063c0-0.956,0.731-1.688,1.688-1.688s1.688,0.731,1.688,1.688 c0,0.394-0.166,0.739-0.334,1.02l-0.616,1.002c-0.056,0.112-0.176,0.341-0.176,0.509v0.281c0,0.619,0.506,1.125,1.125,1.125h4.219  c0.168,0,0.281-0.113,0.281-0.281V13.5c0-0.619,0.506-1.125,1.125-1.125h0.281c0.169,0,0.396,0.063,0.51,0.176 c0,0,0.945,0.616,1.002,0.616c0.337,0.168,0.626,0.334,1.02,0.334c0.956,0,1.687-0.731,1.687-1.687c0-0.957-0.731-1.688-1.687-1.688 c-0.394,0-0.738,0.166-1.02,0.334l-1.002,0.616c-0.113,0.056-0.341,0.176-0.51,0.176H13.5c-0.619,0-1.125-0.506-1.125-1.125V5.908    c0-0.168-0.113-0.281-0.281-0.281H7.875c-0.619,0-1.125-0.506-1.125-1.125V4.221c0-0.168,0.063-0.397,0.176-0.51    c0,0,0.616-0.945,0.616-1.001c0.168-0.281,0.334-0.626,0.334-1.02C7.875,0.733,7.144,0.002,6.188,0.001L6.188,0.001z"/> </svg></i>  
+                <span class="media-body">Data Anggota</span>
+            </a> 
+        </li>
+        <li> 
+            <a class="pmd-ripple-effect" href="{{ url('anggota/index') }}"> 
+                <i class="media-left media-middle"><svg version="1.1" x="0px" y="0px" width="18px" height="12.706px" viewBox="0 0 18 12.706" enable-background="new 0 0 18 12.706" xml:space="preserve"> <path fill="#C9C8C8" d="M0,0v12.706h18V0H0z M12.706,4.235v3.176H9.108V4.235H12.706z M8.049,4.235v3.176h-6.99V4.235H8.049z M1.059,8.47h6.99v3.177h-6.99V8.47z M9.108,11.647V8.47h3.599v3.177H9.108z M13.766,11.647V8.47h3.176v3.177H13.766z M16.942,7.412   h-3.176V4.235h3.176V7.412L16.942,7.412z"/> </svg></i>
+                <span class="media-body">Jadwal Rapat</span>
+            </a> 
+        </li>  
+        <li> 
+            <a class="pmd-ripple-effect" href="{{ url('anggota/index') }}"> 
+                <i class="media-left media-middle"><svg version="1.1" x="0px" y="0px" width="14.187px" height="18px" viewBox="0 0 14.187 18" enable-background="new 0 0 14.187 18" xml:space="preserve"> <path fill="#C9C8C8" d="M0,0v18h14.187V0H0z M3.121,3.293h2.023v4.767H3.121V3.293z M11.211,14.764H2.948v-2.022h8.263V14.764   L11.211,14.764z M11.211,11.585H2.948V9.563h8.263V11.585L11.211,11.585z M11.211,8.407H7.455V6.385h3.756V8.407z M11.211,5.229 H7.455V3.207h3.756V5.229z"/></svg></i>                
+                <span class="media-body">Hasil Rapat</span>
+            </a> 
+        </li>          
+    </ul>
+</aside><!-- End Left sidebar -->
+<!-- Sidebar Ends -->  
+
+@yield('content')
+
+<!-- Footer Starts -->
+<!--footer start-->
+<footer class="admin-footer">
+ <div class="container-fluid">
+    <ul class="list-unstyled list-inline">
+        <li>
+            <span class="pmd-card-subtitle-text">Jurusan Teknik Informatika &copy; <span class="auto-update-year"></span>. All Rights Reserved.</span>
+            <h3 class="pmd-card-subtitle-text">Licensed under <a href="https://opensource.org/licenses/MIT" target="_blank">MIT license.</a></h3>
+        </li>
+        <li class="pull-right for-support">
+            <a href="mailto:mfahmipurnomo@gmail.com">
+                <div>
+                    <svg x="0px" y="0px" width="38px" height="38px" viewBox="0 0 38 38" enable-background="new 0 0 38 38">
+<g><path fill="#A5A4A4" d="M25.621,21.085c-0.642-0.682-1.483-0.682-2.165,0c-0.521,0.521-1.003,1.002-1.524,1.523
+        c-0.16,0.16-0.24,0.16-0.44,0.08c-0.321-0.2-0.683-0.32-1.003-0.521c-1.483-0.922-2.726-2.125-3.809-3.488
+        c-0.521-0.681-1.002-1.402-1.363-2.205c-0.04-0.16-0.04-0.24,0.08-0.4c0.521-0.481,1.002-1.003,1.524-1.483
+        c0.721-0.722,0.721-1.524,0-2.246c-0.441-0.44-0.842-0.842-1.203-1.202c-0.441-0.441-0.842-0.842-1.243-1.243
+        c-0.642-0.642-1.483-0.642-2.165,0c-0.521,0.521-1.002,1.002-1.524,1.523c-0.481,0.481-0.722,1.043-0.802,1.685
+        c-0.08,1.042,0.16,2.085,0.521,3.047c0.762,2.085,1.925,3.849,3.328,5.532c1.884,2.286,4.17,4.05,6.815,5.333
+        c1.203,0.562,2.406,1.002,3.729,1.123c0.922,0.04,1.724-0.201,2.365-0.923c0.441-0.521,0.923-0.922,1.403-1.403
+        c0.682-0.722,0.682-1.563,0-2.245C27.265,22.729,26.423,21.927,25.621,21.085z"/>
+    <path fill="#A5A4A4" d="M32.437,5.568C28.869,2,24.098-0.005,19.005-0.005S9.182,2,5.573,5.568C2.005,9.177,0,13.908,0,19
+        s1.965,9.823,5.573,13.432c3.568,3.568,8.34,5.573,13.432,5.573s9.823-1.965,13.431-5.573
+        C39.854,25.014,39.854,12.985,32.437,5.568z M30.299,30.294c-3.003,3.045-7.021,4.695-11.293,4.695
+        c-4.272,0-8.291-1.65-11.294-4.695C4.666,27.29,3.016,23.271,3.016,19c0-4.272,1.649-8.291,4.695-11.294
+        c3.003-3.003,7.022-4.695,11.294-4.695c4.272,0,8.291,1.649,11.293,4.695C36.56,13.924,36.56,24.075,30.299,30.294z"/>
+</g></svg>
+                </div>
+                <div>
+                  <span class="pmd-card-subtitle-text">For Support</span>
+                  <h3 class="pmd-card-title-text">mfahmipurnomo@gmail.com</h3>
+                </div>
+            </a>
+        </li>
+        
+    </ul>
+ </div>
+</footer>
+<!--footer end-->
+<!-- Footer Ends -->
+@yield('javascript')
+</body>
+</html>
